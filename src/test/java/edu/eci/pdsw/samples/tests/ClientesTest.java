c2a51e77d8c027df1c3acffd056da6cc7e2627a4
cesareduardo824 <2098325@linux22.labinfo.is.escuelaing.edu.co>
2018-02-15 12:53:06 -0500

    punto 10, subpunto1, falta revisar clases de equivalencia

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -5,7 +5,10 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquilerItemsStub;
+import java.util.LinkedList;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -29,8 +32,34 @@ public class ClientesTest {
     	
     }
     
-    
-    
+    /**
+     * /**
+     * @obj registrar un cliente en el sistema
+     * @param p el nuevo cliente
+     * @pre p!=null
+     * @pos el cliente queda disponible para futuros alquileres
+     * @throws ExcepcionServiciosAlquiler si el cliente ya se encuentra registrado
+     *
+    public abstract void registrarCliente(Cliente p) throws ExcepcionServiciosAlquiler;
+     *  Clases de equivalencia
+     * El cliente no sea nulo, entonces se generara un nuevo cliente.
+     * SI el cliente es nulo genera excepcion.
+     * 
+     * 
+     */
+    @Test
+    public void regClientes()throws ExcepcionServiciosAlquiler{
+        LinkedList<Cliente> lisPru= new LinkedList<>();
+        ServiciosAlquilerItemsStub pru= new ServiciosAlquilerItemsStub();
+        Cliente clie=new Cliente("Pepito", 1111111, "1111111", "Calle 1 # 1-1", "1@1mail.com");
+        
+        pru.registrarCliente(clie);
+        lisPru=(LinkedList<Cliente>) pru.consultarClientes();
+        
+        assertTrue(lisPru.contains(clie));
+        
+     
+    }
     
     
     
